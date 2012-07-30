f52de4eeb76e948571ed0c85935b68b7d0888a17
alex-ida <alex@idamob.ru>
2012-07-30 22:07:43 +0400

    Initial Commit

---
+++ AZImagePickerViewController/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AZImagePickerViewController
+//
+//  Created by Alex Zinchenko on 30/07/2012.
+//  Copyright (c) 2012 Alex Zinchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
